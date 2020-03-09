dd8a22f18830ec246f634267c337eb2210fd86aa
Lukas Tveiten <lukasnt@stud.ntnu.no>
2020-03-09 12:06:17 +0100

    Added so that you can input user and password in the DBConn

--- databaseprosjekt/src/main/DBConn.java
+++ databaseprosjekt/src/main/DBConn.java
@@ -1,7 +1,9 @@
 package main;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class DBConn {
 	
@@ -13,15 +15,23 @@ public class DBConn {
     
     public void connect() {
     	try {
+    		Scanner scanner = new Scanner(System.in);
+    		System.out.println("user: ");
+    		String user = scanner.nextLine();
+    		System.out.println("password: ");
+    		String password = scanner.nextLine();
+    		System.out.println("Schema Name: ");
+    		String schemaName = scanner.nextLine();
+    		
             //Class.forName("com.mysql.jdbc.Driver").newInstance();
             Class.forName("com.mysql.cj.jdbc.Driver"); // when you are using MySQL 8.0.	    
             // Properties for user and password.
             Properties p = new Properties(); // 123
-            p.put("user", "root");
-            p.put("password", "tomat123");           
+            p.put("user", user);
+            p.put("password", password);           
             // conn = DriverManager.getConnection("jdbc:mysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
-            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/filmprosjekt?autoReconnect=true&useSSL=false&serverTimezone=UTC", p);
-        } catch (Exception e)
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1/" + schemaName + "?autoReconnect=true&useSSL=false&serverTimezone=UTC", p);
+    	} catch (Exception e)
     	{
             throw new RuntimeException("Unable to connect", e);
     	}
